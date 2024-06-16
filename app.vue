720daa7c360ffa0ddc5aab88e9d69eab4d520e5b
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2024-06-16 16:29:13 +0800

    Added basic theming

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="p-4">
-    <button class="btn">Hello daisyUI</button>
+  <div>
+    <!-- Toggle switch -->
+    <div class="absolute top-0 right-0 m-4 p-2">
+      <label class="swap swap-flip">
+        <input type="checkbox" class="toggle" @click="toggleTheme" />
+      </label>
+    </div>
+
+    <!-- Your app content here -->
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      theme: 'dark'
+    };
+  },
+  mounted() {
+    // Initially set the theme
+    this.setTheme(this.theme);
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'dark' ? 'light' : 'dark';
+      this.setTheme(this.theme);
+    },
+    setTheme(theme) {
+      document.documentElement.setAttribute('data-theme', theme);
+    }
+  }
+};
+</script>
